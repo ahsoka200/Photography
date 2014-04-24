227d3a8d67b708b578692cea98824c4008e9b20e
ahsoka200 <ahsoka200@gmail.com>
2014-04-24 12:22:18 -0700

    “

--- static/photo-category.js
+++ static/photo-category.js
@@ -6,12 +6,12 @@ $(window).load(function(){
         // for the images with a fluid width and height 
         responsive:true,
         // to invert the hover effect
-        invertHoverEffect: false,
+        invertHoverEffect: true,
         // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)
         intensity:1,
         speed: { //this property could also be just speed: value for both fadeIn and fadeOut
-            fadeIn: 200, // 200ms for fadeIn animations
-            fadeOut: 800 // 800ms for fadeOut animations
+            fadeIn: 400, // 200ms for fadeIn animations
+            fadeOut: 300 // 800ms for fadeOut animations
         },
         onImageReady:function(img) {
         	// this callback gets executed anytime an image is converted
