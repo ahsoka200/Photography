c45dba3f2374922d13ffc9a2e7be7de0da24c022
ahsoka200 <ahsoka200@gmail.com>
2014-05-01 11:08:23 -0700

    “

--- static/image-replacement.js
+++ static/image-replacement.js
@@ -2,6 +2,10 @@
 var bottomImages = $(".bottomImages img");
 var mainimage = $("img.mainimage");
 
+bottomImages.first().addClass('border');
+
 bottomImages.on( "click", function( event ) {
    mainimage.attr('src', $(this).attr('src'));
+   bottomImages.removeClass('border');
+   $(this).addClass('border');
 });
