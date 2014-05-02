2da052ea8b88966519867a23e94bbed3d70f21e5
ahsoka200 <ahsoka200@gmail.com>
2014-05-01 18:09:34 -0700

    “

--- static/photo-category-grey-filter.js
+++ static/photo-category-grey-filter.js
@@ -3,12 +3,12 @@ var picture = $(".bwWrapper");
 
 
 picture.mouseover(function() {
-	var greyFilter = $( this ).find( "div" );
+	var greyFilter = $( this ).find( "div.photoCategoryFilter" );
     greyFilter.fadeIn(400);
 });
 
 
 picture.mouseleave(function() {
-  var greyFilter = $( this ).find( "div" );
+  var greyFilter = $( this ).find( "div.photoCategoryFilter" );
   greyFilter.fadeOut(200);
 });
