ea8d469a1bb667b45826cefc76df7df989e9f642
ahsoka200 <ahsoka200@gmail.com>
2014-05-27 09:16:15 -0700

    “

--- static/info-box.js
+++ static/info-box.js
@@ -8,6 +8,8 @@ var leftarrow = $(".leftarrow");
 rightarrow.on( "click", function( event ) {
 	
 	text.fadeOut(400, function() {
+		infodiv.css('min-height', '0px');
+		infodiv.css('min-width', '0px');
 		infodiv.animate({ "height": "50px", "width": "50px" }, 1000 );
 		leftarrow.show();
 		rightarrow.hide();
@@ -22,11 +24,13 @@ rightarrow.on( "click", function( event ) {
 
 leftarrow.on( "click", function( event ) {
 	infodiv.animate({ "height": "189px", "width": "170px" }, 1000, function() {
-
-		text.fadeIn(400);
+	infodiv.css('min-height', '220px');
+	infodiv.css('min-width', '200px');
+	text.fadeIn(400);
 
 	leftarrow.hide();
 	rightarrow.show();
+	infodiv.css('height', 'auto');
 });
 	
 
