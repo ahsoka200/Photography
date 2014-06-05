8bdfc1723a81c1fb12264ae4502986b1996819d5
ahsoka200 <ahsoka200@gmail.com>
2014-06-05 11:41:49 -0700

    “

--- databasecreation.py
+++ databasecreation.py
@@ -53,6 +53,13 @@ image9= PhotoInfo('Up', 'Los Altos', '2013', 'IMG_9599.jpg', 'lifestyle')
 db.session.add(image9)
 db.session.commit()
 
+imageTEST= PhotoInfo('TEST', 'Hawaii Oahu', '2014', 'Max on the beach 2.jpg', 'lifestyle')
+db.session.add(image9)
+db.session.commit()
+
+
+
+
 #sports below
 
 image10= PhotoInfo('Surf', 'Oahu', '2014', 'G0020768.JPG', 'sports')
